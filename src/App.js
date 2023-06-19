205da3dd31b18ee0f6bfbe953f3a05916ddd8590
oli-viaaaa <4688ehowl@naver.com>
2023-06-19 13:30:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // jsx에 들어갈 변수 선언과 값 할당
+  const name = "마이크";
+  const person = {
+    name : "길동",
+    age : 20
+  }
+
   return (
-    <div className="App">
-      <h1>안녕하세요</h1>
-    </div>
+    // App.css 파일에 있는 App 이라는 클래스 속성을 나타냄
+    <>
+      <div className="App">
+        <h1>안녕하세요</h1>
+        <h2>나의 첫번째 리액트 앱</h2>
+        <button onClick={() => console.log('버튼 클릭')}>
+          Click me
+        </button>
+
+        <input type="text" placeholder='텍스트를 입력하세요.'/>
+          <select>
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+      </div>
+
+      <div className='App'>
+        <h1 style={{color:"#f0f", backgroundColor:"green"}}>하이 제인</h1>
+        <h1 style={{color:"#ff0", backgroundColor:"blue"}}>하이 {name}</h1>
+        <h1 style={{color:"#0ff", backgroundColor:"red"}}>연산결과 {3+5}</h1>
+      </div>
+
+      {/*{중괄호} 안에 컴포넌트에서 할당한 변수값을 출력한다.*/}
+      <div className='App'>
+        <h1>{person.name}</h1>
+        <h1>{person.age}</h1>
+      </div>
+    </>
+    
   );
 }
-
+/*
+  App 컴포넌트를 이 파일의 기본 반환값으로 하겠다
+  이렇게 내보낸 컴포넌트는 다른 컴포넌트에서 import해서 사용할 수 있다.
+*/
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // 'react' 패키지에서 React를 가져오는 구문
+// 리액트 컴포넌트를 작성할 때 필요한 기능을 가져오기 위한 import 문이다.
+// React는 리액트 라이브러리의 핵심 기능을 포함하는 객체이다.
+// 이를 사용하여 리액트 컴포넌트를 작성하고, JSX를 렌더링하고 기타 리액트 관련 기능을 활용할 수 있다.
 import React from 'react';
 // node_modules 폴더에서 'react-dom' 패키지의 ReactDOM 객체를 가져오는 구문이다.
 // ReactDOM은 React 애플리케이션을 실제 DOM에 렌더링 하는 역할
@@ -27,7 +30,7 @@ root.render(
 );
 
 // 위의 두 줄을 줄여서 다음과 같이 사용 가능
-ReactDOM.render(<App />, document.getElementById('root'))
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
